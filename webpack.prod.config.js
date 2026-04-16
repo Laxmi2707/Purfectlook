54d90ca142f98b9347844b4c33d587f6fb796935
Laxmi Prajapati <prajapatilaxmi2707@gmail.com>
2026-04-16 11:31:53 +0530

    Update webpack.prod.config.js

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,5 +1,45 @@
-output: {
-  filename: 'js/[name].js',
-  path: path.resolve(__dirname, '../dist'),
-  clean: true
-}
+const path = require('path');
+const { merge } = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const common = require('./webpack.dev.config'); // reuse your current config
+
+module.exports = merge(common, {
+  mode: 'production',
+
+  output: {
+    filename: 'js/[name].[contenthash].js',
+    path: path.resolve(__dirname, '../dist'),
+    clean: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.((c|sa|sc)ss)$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash].css',
+    }),
+  ],
+
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin(),
+      new CssMinimizerPlugin(),
+    ],
+  },
+});
